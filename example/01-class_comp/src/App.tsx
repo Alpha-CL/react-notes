fa7a72b2a436c8f428157028113d19a8719f1230
xiaoodao_92 <xiaodao_92@yeah.net>
2022-07-26 09:38:04 +0800

    test

--- example/01-class_comp/src/App.tsx
+++ example/01-class_comp/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import DemoComp from "./pages/demo";
 
 function App() {
   return (
     <div className="App">
+        <DemoComp />
     </div>
   );
 }
 
 export default App;
-
